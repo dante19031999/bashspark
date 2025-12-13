55a267891e0cc05aeec3439b8bd0f593f4e3efa3
dante19031999 <74316278+dante19031999@users.noreply.github.com>
2025-12-13 14:42:57 +0100

    Fix license header on command_math.cpp

--- src/BashSpark/command/command_math.cpp
+++ src/BashSpark/command/command_math.cpp
@@ -1,6 +1,28 @@
-//
-// Created by dante on 2/12/25.
-//
+/**
+ * @file command_math.cpp
+ * @brief Implements command `bb::command_math`.
+ *
+ * @date Created on 2/11/25
+ * @author Dante Doménech Martínez
+ *
+ * @copyright GNU General Public License v3.0
+ *
+ * This file is part of BashBabel.
+ * Copyright (C) 2025 Dante Doménech Martínez
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "BashSpark/command/command_math.h"
 
